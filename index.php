7b1327a6327681bc160a24b06da5ff068d86c8aa
Christian Hernandez <christianh814@users.noreply.github.com>
2016-01-07 14:20:27 -0800

    Update index.php

--- index.php
+++ index.php
@@ -198,7 +198,7 @@ line-height: 1.4;
 
 <section class='container'>
           <hgroup>
-            <h1>PHP application on OpenShift</h1>
+            <h1>PHP application on OpenShift - DEV ONLY</h1>
           </hgroup>
 
 <?php
